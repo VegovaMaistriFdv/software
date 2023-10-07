e253430c5e50f96855af99629fad04a87df7f7a4
JakobPuc <titan.lol.com@gmail.com>
2023-10-07 12:42:00 +0200

    dela parsanje v datoteke

--- poskusscrapanjagpt.py
+++ poskusscrapanjagpt.py
@@ -1,11 +1,11 @@
 import requests
 from datetime import date
-import urllib
-import os
+
 
 today = date.today()
+print(today)
 
-url = 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=87.509706297451&bounds%5Bnwlon%5D=-360&bounds%5Bselat%5D=-84.89714695160268&bounds%5Bselon%5D=540.0000000000001&zoom=2&date=2023-07-01&date_end=2023-07-31&indexname=s3fires_solar_zenith&satellite=S3A'
+url = 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=87.509706297451&bounds%5Bnwlon%5D=-360&bounds%5Bselat%5D=-84.89714695160268&bounds%5Bselon%5D=540.0000000000001&zoom=2&date=%sdate_end=2023-07-31&indexname=s3fires_solar_zenith&satellite=S3A' ,(today)
 r = requests.get(url, allow_redirects=True)
 
 open('.\esa\dat.cvs', 'wb').write(r.content)
@@ -37,7 +37,6 @@ while True:
     sirina1.append(sirina)
     dolzina = line[x2+1: ]
     dolzina1.append(dolzina)
-
 
 
 
