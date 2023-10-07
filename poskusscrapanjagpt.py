c0c7937d93fb7baaefd181011182249c00f49bdb
JakobPuc <titan.lol.com@gmail.com>
2023-10-07 14:44:39 +0200

    dela parsanje v datoteke

--- poskusscrapanjagpt.py
+++ poskusscrapanjagpt.py
@@ -1,14 +1,33 @@
 import requests
-import time
 import datetime 
+import pandas as pd
 
 
 today = datetime.date.today()
+today = pd.to_datetime(datetime.date.today(), format="%Y-%m-%d") - pd.DateOffset(days=3)
+today = str(today)
+today =today[0:10]
+today = str(today)
 print(today)
 
-#url = 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=87.509706297451&bounds%5Bnwlon%5D=-360&bounds%5Bselat%5D=-84.89714695160268&bounds%5Bselon%5D=540.0000000000001&zoom=2&date='+today+'date_end=2023-07-31&indexname=s3fires_solar_zenith&satellite=S3A'
-r = requests.get(url, allow_redirects=True)
 
+
+today_30 = pd.to_datetime(datetime.date.today(), format="%Y-%m-%d") - pd.DateOffset(months=1)
+today_30 =str(today_30)
+today_30 =today_30[0:10]
+today_30 =str(today_30)
+print(today_30)
+
+
+
+
+#url= 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=87.509706297451&bounds%5Bnwlon%5D=-360&bounds%5Bselat%5D=-84.89714695160268&bounds%5Bselon%5D=540.0000000000001&zoom=2&date='+today+'&date_end='+today_30+'&indexname=s3fires_solar_zenith&satellite=S3A'
+url= 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=81.72318761821157&bounds%5Bnwlon%5D=-247.50000000000003&bounds%5Bselat%5D=-73.22669969306126&bounds%5Bselon%5D=427.50000000000006&zoom=2&date=' + today_30 + '&date_end=' + today + '&indexname=s3fires_solar_zenith&satellite=S3A'
+#url= 'https://s3wfa.esa.int/api/csv-export?bounds%5Bnwlat%5D=81.72318761821157&bounds%5Bnwlon%5D=-247.50000000000003&bounds%5Bselat%5D=-73.22669969306126&bounds%5Bselon%5D=427.50000000000006&zoom=2&date=2023-09-07&date_end=2023-10-04&indexname=s3fires_solar_zenith&satellite=S3A'
+
+#print(url)
+#url = str(url)
+r = requests.get(url, allow_redirects=True)
 open('.\esa\dat.cvs', 'wb').write(r.content)
 
 file = open('.\esa\dat.cvs', 'r')
@@ -36,7 +55,7 @@ while True:
                 x2 = x
     sirina = line[x1:x2]
     sirina1.append(sirina)
-    dolzina = line[x2+1: ]
+    dolzina = line[x2+1:-1]
     dolzina1.append(dolzina)
 
 
